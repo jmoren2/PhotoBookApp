41b54bf2be6b21f7ead1c62f083f7a811779a353
Josh Moreno <jmoreno@navexglobal.com>
2021-01-18 21:24:00 -0800

    albums create and list done

--- photobook/src/store/index.js
+++ photobook/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { auth } from '../store/auth/auth'
+import { albumInfo } from '../store/albums/albums'
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    albumInfo
   }
 })
